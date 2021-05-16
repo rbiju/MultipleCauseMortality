6201b0c78a89fd1700838a987023709a3defd9cf
rbiju <raymondbiju@gmail.com>
2021-05-16 02:09:41 -0500

    MSE Approach:
    Previous approach approached next word prediction as a classification problem. Here an attempt is made to use mean squared error or a related concept as the loss function between machine guesses and target embedding vectors. Output layer resembles word embedding rather than categorical softmax.

--- LSTM_dataPrep.py
+++ LSTM_dataPrep.py
@@ -99,7 +99,7 @@ def makeMotherList(dataframe, train_len, threshold):
     # word2vec model training
     print('\nTraining Word2Vec.\n')
     sentences = MyCorpus()
-    w2v_model = gensim.models.Word2Vec(sentences=sentences, sg=1, min_count=2, vector_size=32)
+    w2v_model = gensim.models.Word2Vec(sentences=sentences, sg=1, min_count=2, vector_size=9)
     w2v_model.init_sims(replace=True)
     w2v_model.save("word2vec.model")
     print('\nWord2Vec Trained.\n')
